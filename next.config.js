d5d401c495bbbb843c09fcfbea3b60d109679f9c
tuniss1 <77372423+tuniss1@users.noreply.github.com>
2022-12-01 11:33:46 +0700

    Config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "firebasestorage.googleapis.com",
+      "i.pinimg.com",
+      "pondinformer.com",
+      "s3.amazonaws.com",
+      "www.thespruce.com",
+      "hort.extension.wisc.edu",
+      "www.thespruce.com",
+    ],
+  },
 };
---
+++ src/__mocks__/order.js
@@ -0,0 +1,221 @@
+export const orders = [
+  {
+    _id: "63804b51e5347db5593f3334",
+    userId: "637455c4f7c0345fb31836aa",
+    totalCost: 100000,
+    note: "",
+    status: 5,
+    products: [
+      {
+        _id: "636daaec21bcdaf83b65b575",
+        name: "Cá",
+        price: 40000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "636dce37880a68818093c51b",
+        name: "Cá heo",
+        price: 12000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "636dce95880a68818093c51e",
+        name: "Cá heo",
+        price: 12000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "636dcea1880a68818093c521",
+        name: "Cá heo",
+        price: 12000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "636dceab880a68818093c524",
+        name: "Cá heo",
+        price: 12000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "637208df660bf4189689227b",
+        name: "Food",
+        price: 40000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+    ],
+    deliver: {
+      location: "123 Hậu Giang, q6",
+      fee: 12000,
+    },
+    createdAt: 1669350997097,
+    __v: 0,
+    customer: {
+      firstName: "Dat",
+      lastName: "Vo",
+    },
+  },
+  {
+    _id: "63804b52e5347db5593f3337",
+    userId: "637455c4f7c0345fb31836aa",
+    totalCost: 100000,
+    note: "",
+    status: 5,
+    products: [
+      {
+        _id: "636daaec21bcdaf83b65b575",
+        name: "Cá",
+        price: 40000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "636dce37880a68818093c51b",
+        name: "Cá heo",
+        price: 12000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "636dce95880a68818093c51e",
+        name: "Cá heo",
+        price: 12000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "636dcea1880a68818093c521",
+        name: "Cá heo",
+        price: 12000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "636dceab880a68818093c524",
+        name: "Cá heo",
+        price: 12000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "637208df660bf4189689227b",
+        name: "Food",
+        price: 40000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+    ],
+    deliver: {
+      location: "123 Hậu Giang, q6",
+      fee: 12000,
+    },
+    createdAt: 1669350997097,
+    __v: 0,
+    customer: {
+      firstName: "Dat",
+      lastName: "Vo",
+    },
+  },
+  {
+    _id: "63804b54e5347db5593f333a",
+    userId: "637455c4f7c0345fb31836aa",
+    totalCost: 100000,
+    note: "",
+    status: 2,
+    products: [
+      {
+        _id: "636daaec21bcdaf83b65b575",
+        name: "Cá",
+        price: 40000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "636dce37880a68818093c51b",
+        name: "Cá heo",
+        price: 12000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "636dce95880a68818093c51e",
+        name: "Cá heo",
+        price: 12000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "636dcea1880a68818093c521",
+        name: "Cá heo",
+        price: 12000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "636dceab880a68818093c524",
+        name: "Cá heo",
+        price: 12000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+      {
+        _id: "637208df660bf4189689227b",
+        name: "Food",
+        price: 40000,
+        quantity: 1,
+        images: [
+          "https://s3-alpha-sig.figma.com/img/0d10/adea/e20d65d027f5ea481f74ed8a81ed052b?Expires=1669593600&Signature=MGGuSrylIrvo0rqlGjAlpxtwedq4oUNkD20LppowdBsvnwdmEvFAGX7YyGagot7~NFiqHTbD-jLQOaZ3hpfgWsbzdlDpp~DjHzW4v0M4Q1fvc2I~NM-Ov2EZzRldE9nXI4yvYLyjVLZAW~xASlvbgOUTTYmglM1pNk3vVTL7VCQ4ma-f2Jq7Hzw51nPzWFWi94VobjJgL4an4I~9w~Zlk8uMRCPKdeNHaBYmxtVPgRw3Whn5mEFQaCY2qXLgogH1WpBE-5mgNB56VpWFJgXUeIQZSwl6f4rVXECAsk5k2ZzJ-pMZrlYKOg06mf0Sco8rympjzt3AVppTTV9byR7Yng__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+        ],
+      },
+    ],
+    deliver: {
+      location: "123 Hậu Giang, q6",
+      fee: 12000,
+    },
+    createdAt: 1669350997097,
+    __v: 0,
+    customer: {
+      firstName: "Dat",
+      lastName: "Vo",
+    },
+  },
+];
