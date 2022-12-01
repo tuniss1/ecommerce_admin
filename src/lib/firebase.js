9e2d01f02c6bddf58af5039ff67ad70f509caa78
tuniss1 <77372423+tuniss1@users.noreply.github.com>
2022-12-01 11:31:21 +0700

    Config firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDTmVEGX6A0Kb2IxIm4DFrDvyE1MP4n4Rw",
+  authDomain: "ecommerce-dragon-fish.firebaseapp.com",
+  projectId: "ecommerce-dragon-fish",
+  storageBucket: "ecommerce-dragon-fish.appspot.com",
+  messagingSenderId: "705987834758",
+  appId: "1:705987834758:web:f4ebda0c639070c850f207",
+  measurementId: "G-YZFWV2PMSK",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const storage = getStorage(app);
