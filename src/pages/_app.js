33cc8700a6e5fa7cf9e9765f1f9e9de7d46d8ce8
tuniss1 <77372423+tuniss1@users.noreply.github.com>
2022-12-12 12:43:36 +0700

    Update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -25,7 +25,7 @@ const App = (props) => {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>Material Kit Pro</title>
+        <title>Dragon Fish Admin</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
